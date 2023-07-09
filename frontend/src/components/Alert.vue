76ff3103facc7f427673cdfa07f0aadc3f5b8f47
LaGabor <lagaborka@gmail.com>
2023-07-09 10:43:06 +0200

    feat:Alert functionality created for creating messages after (un)successful reservations.

---
+++ frontend/src/components/Alert.vue
@@ -0,0 +1,35 @@
+<template>
+  <div v-if="booked === 'sold'" class="alert alert-danger alert-dismissible fade show" role="alert">
+    The Seat already sold out for this date!
+    <button type="button" class="btn-close" @click="dismissAlert" aria-label="Close"></button>
+  </div>
+  <div v-else-if="booked === 'reserved'" class="alert alert-secondary alert-dismissible fade show" role="alert">
+    At the moment this seat reserved for this date!
+    <button type="button" class="btn-close" @click="dismissAlert" aria-label="Close"></button>
+  </div>
+  <div v-else-if="booked === 'timeout'" class="alert alert-warning alert-dismissible fade show" role="alert">
+    At the moment this seat reserved for this date!
+    <button type="button" class="btn-close" @click="dismissAlert" aria-label="Close"></button>
+  </div>
+  <div v-else-if="booked === 'booked'" class="alert alert-success alert-dismissible fade show" role="alert">
+      You have successfully reserved your seat.
+    <button type="button" class="btn-close" @click="dismissAlert" aria-label="Close"></button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Alert",
+  props:['booked'],
+  methods: {
+
+    dismissAlert(){
+      this.$emit("dismissAlert");
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
