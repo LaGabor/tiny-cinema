5d80a9e062c6c80d45d544f7e013489b3752ab19
LaGabor <lagaborka@gmail.com>
2023-07-09 10:29:26 +0200

    feat:Routes created for get back reservation status by date, reserve seat, and finalize seats reservation.

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ReservationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,17 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/seats/date',[ReservationController::class,'getSeatsByDate']);
+Route::post('/seat/reserve',[ReservationController::class,'reserveSeat']);
+Route::put('/seats/buy',[ReservationController::class,'buySeats']);
+
+
+
+
+
+
+
+
+
