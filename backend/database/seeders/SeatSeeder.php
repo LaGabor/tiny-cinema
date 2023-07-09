06133bd6e2e785d40a077643327ab7c388fd6ef9
LaGabor <lagaborka@gmail.com>
2023-07-09 10:25:51 +0200

    feat:Seat seeder functionality created.

--- backend/database/seeders/SeatSeeder.php
+++ backend/database/seeders/SeatSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Seat;
 
 class SeatSeeder extends Seeder
 {
@@ -12,6 +13,14 @@ class SeatSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Seat::truncate();
+
+        Seat::create([
+            'id' => 1,
+        ]);
+
+        Seat::create([
+            'id' => 2,
+        ]);
     }
 }
